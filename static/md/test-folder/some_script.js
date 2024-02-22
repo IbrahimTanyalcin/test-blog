c5c0ac0c76ab9b927bfbe8917ea29d6178c2817e
Ibrahim Tanyalcin <ibowankenobi@ibrahimtanyalcin.com>
2024-02-22 21:31:59 +0100

    Update some_script.js

--- static/md/test-folder/some_script.js
+++ static/md/test-folder/some_script.js
@@ -1,8 +1,6 @@
-<script data-some-attr="look">
-  !function(){
-    const prefix = document.currentScript.getAttribute("data-some-attr");
-    document.querySelector(".text-container-1").textContent = `
-      ${prefix} this is a script that is being executed. So be careful not 
-      accept 'any' PR. If you are unsure of the document, consider purification.`
-  }()
-</script>
+!function(){
+  const prefix = "LOOK!";
+  document.querySelector(".text-container-1").textContent = `
+    ${prefix} this is a script that is being executed. So be careful not 
+    accept 'any' PR. If you are unsure of the document, consider purification.`
+}()
